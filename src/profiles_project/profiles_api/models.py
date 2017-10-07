61c23e162e6bc7c6cc739c2ca47797473202a51c
prasad.sawantdesai <prasad.sawantdesai@gmail.com>
2017-10-07 09:51:05 +0530

    added a custom user profile model, manager and ,migration for our database

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
+class UserProfileManager(BaseUserManager):
+    """Helps Django work with our custom user model"""
+
+    def create_user(self,email, name, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email=self.normalize_email(email)
+
+        user = self.model(
+            email=email,
+            name=name
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        """
+        Creates and saves a superuser with the given email, name and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            name=name,
+        )
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Represents a "user profile" inside our system"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects =  UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Used to get user's full name"""
+
+        return self.name
+
+    def get_short_name(self):
+        """Used to get user's short name"""
+
+        return self.name
+
+
+    def __str__(self):
+        """Uses this when convert object to string"""
+
+        return self.email
