9558e142bd9d80924ab9be8bea00f8455dcee1c0
prasad.sawantdesai <prasad.sawantdesai@gmail.com>
2017-10-13 09:44:01 +0530

    added model for storing Profile Feed item model

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -59,8 +59,19 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
         return self.name
 
-
     def __str__(self):
         """Uses this when convert object to string"""
 
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+    """ profiles status update"""
+    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Returns model as string"""
+
+        return self.status_text
